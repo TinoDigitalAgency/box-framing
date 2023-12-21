89208a2461781bf7de381f4301b737dde97f2042
1an21 <tino.agency@gmail.com>
2023-12-21 09:12:17 +0200

    make width and height dynamic

--- App.tsx
+++ App.tsx
@@ -1,18 +1,17 @@
-import {StatusBar, StyleSheet, View} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import Box from "./Box";
-import Logo from "./Logo";
 
 export default function App() {
   return (
     <>
       <StatusBar backgroundColor={'black'} translucent={true}/>
-      <View style={styles.container}>
-        <Logo/>
-        <Box width={300} height={300}/>
-        {/*<Box width={300} height={200}>*/}
-        {/*  <Box width={276} height={90} withoutInner/>*/}
-        {/*</Box>*/}
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Box>
+          <Text style={{color: '#fff', fontSize: 20, lineHeight: 25}}>Test text</Text>
+          <View style={{height: 200}}/>
+          <Text style={{color: '#fff', fontSize: 20, lineHeight: 25}}>Test text</Text>
+        </Box>
+      </SafeAreaView>
     </>
   );
 }
@@ -22,8 +21,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#09090C',
     paddingHorizontal: 24,
+    paddingVertical: 50,
     alignItems: 'center',
     justifyContent: 'center',
-    gap: 20
+    gap: 20,
   },
 });
--- Box.tsx
+++ Box.tsx
@@ -1,19 +1,30 @@
-import {StyleSheet, View} from "react-native";
-import React from "react";
+import {Dimensions, StyleSheet, View} from "react-native";
+import React, {useState} from "react";
 // @ts-ignore
 import {ShadowBox} from 'react-native-neomorph-shadows';
 import {LinearGradient} from "expo-linear-gradient";
 
-const Box = ({children, width, height, withoutInner}) => {
+const Box = ({children}) => {
+
+  const [parentDimensions, setParentDimensions] = useState({width: 0, height: 0});
+
+  const onParentLayout = (event) => {
+    const {width, height} = event.nativeEvent.layout;
+    if (parentDimensions.height === 0 && parentDimensions.width === 0) {
+      setParentDimensions({width, height});
+    }
+  };
+
   return (
     <LinearGradient
       colors={['#FC423380', '#FFEE92', '#FC423380']}
-      style={[styles.boxBorder, {width: width, height: height}]}
+      style={[styles.boxBorder, parentDimensions.height && {height: parentDimensions.height}]}
       start={{x: 0, y: 1}}
       end={{x: 1, y: 1}}
+      onLayout={onParentLayout}
     >
       <ShadowBox
-        inner={!withoutInner}
+        inner
         useSvg
         style={{
           shadowOffset: {width: 0, height: 0},
@@ -21,10 +32,13 @@ const Box = ({children, width, height, withoutInner}) => {
           shadowColor: "#FF7F00",
           shadowRadius: 30,
           borderRadius: 16,
-          width: width - 2,
-          height: height - 2,
+          width: parentDimensions.width - 2,
+          height: parentDimensions.height - 2,
         }}>
-        <View style={styles.box}>
+        <View style={[styles.box, {
+          width: parentDimensions.width - 2,
+          height: parentDimensions.height - 2
+        }]}>
           {children}
         </View>
       </ShadowBox>
@@ -34,21 +48,21 @@ const Box = ({children, width, height, withoutInner}) => {
 
 const styles = StyleSheet.create({
   boxBorder: {
+    width: Dimensions.get('window').width - 48,
     borderRadius: 16,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   boxShadow: {
     borderRadius: 16,
   },
   box: {
     width: '100%',
-    height: '100%',
     backgroundColor: '#14141B',
     borderRadius: 16,
-    justifyContent: "flex-end",
+    justifyContent: "center",
     alignItems: "center",
-    padding: 8
+    paddingVertical: 16
   }
 });
 
